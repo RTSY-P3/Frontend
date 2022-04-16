94b594d7b23f0d27d2067c9ae1a9c823a9fa8c1c
ryan.ditzel <ryand370@gmail.com>
2022-04-16 13:11:07 -0400

    bootstrapped Footer.jsx

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+    return (
+        <div className='Footer'>
+            <div className='LeftSide-footer'>
+
+            </div>
+            <div className='Middle-footer'>
+
+            </div>
+            <div className='RightSide-footer'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Footer
