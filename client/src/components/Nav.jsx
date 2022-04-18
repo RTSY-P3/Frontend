21dcbe8f54c6676946c74d1bd0e5abdfccc813b2
ryan.ditzel <ryand370@gmail.com>
2022-04-18 10:45:00 -0400

    made changes to Nav.jsx

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,27 +1,42 @@
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
-import '../styles/nav.css'
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import "../styles/nav.css";
 
-const Nav = () => {
+const Nav = ({ authenticated, user, handleLogOut }) => {
+  const [showLinks, setShowLinks] = useState(false);
 
-    const [ showLinks, setShowLinks ] = useState(false)
-return (
+  let authenticatedOptions;
+  if (user) {
+    authenticatedOptions = (
+      <nav>
+        <h3>Welcome {user.email}!</h3>
+        <Link to="/feed">Feed</Link>
+        <Link onClick={handleLogOut} to="/">
+          Sign Out
+        </Link>
+      </nav>
+    );
+  }
 
-    <div className='nav-bar'>
-        <div className='LeftSide'>
-            <Link to='/'>Codr.</Link>
-        </div>
-        <div className='RightSide'>
-            <div className='nav-link' id={showLinks ? 'hidden' : ''}>
-                <Link to='/credit'>Credit</Link>
-                <Link to=''>Resources</Link>
-                <Link className='signup' to=''>Sign Up</Link>       
-                <Link className='signin' to=''>Log In</Link>
-            </div>
-            <button className='navbutton' onClick={() => setShowLinks(!showLinks) }></button>
-        </div>
+  const publicOptions = (
+    <nav className="nav-link">
+      <Link className="LeftSide" to="/">Codr.</Link>
+      <Link className="RightSide" to="">Resources</Link>
+      <Link className="RightSide signup" to="/register">Sign Up</Link>
+      <Link className="RightSide signin" to="/signin">Log In</Link>
+    </nav>
+  );
+  return (
+    <div className="nav-bar">
+        <div className="nav-link" id={showLinks ? "hidden" : ""}>  
+        <button
+          className="navbutton"
+          onClick={() => setShowLinks(!showLinks)}
+        ></button>
+        {authenticated && user ? authenticatedOptions : publicOptions}
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default Nav
+export default Nav;
