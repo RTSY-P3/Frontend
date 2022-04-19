d5ae3824b80efc8e01b907f9f894b260a3d566c5
Yusong Shi <jackshi.sys@gmail.com>
2022-04-19 10:12:33 -0400

    auth.js update

--- client/src/pages/MyProfile.js
+++ client/src/pages/MyProfile.js
@@ -3,7 +3,7 @@ import React from "react"
 const MyProfile = () => {
     return (
         <div className="my-profile">
-            this is my profile
+            <h1>this is my profile</h1>
         </div>
     )
 }
