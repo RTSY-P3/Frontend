6e3efbbf53421539ed97984987858272dc44a1cd
ryan.ditzel <ryand370@gmail.com>
2022-04-22 10:04:52 -0400

    added modal for edit projects

--- client/src/components/UpdateProjectButton.jsx
+++ client/src/components/UpdateProjectButton.jsx
@@ -31,12 +31,14 @@ const UpdateProjectBtn = ( { post } ) => {
             {update ?
             <form className="col" onSubmit={handleSubmit}>
                 <div className="update-close-button">
-                    <button className="submit-button" type='submit'>Update</button>
                     <button className='close-update-button' onClick={() => setUpdate(false)}>&times;</button>
                 </div>
-                <input className="create-update" name='title' type='text' placeholder='Title Here' onChange={handleChange} value={formValues.title}></input>
+                <input className="create-update-title" name='title' type='text' placeholder='Title Here' onChange={handleChange} value={formValues.title}></input>
                 <textarea className="create-update" name='body' type='text' placeholder='Body Text' onChange={handleChange} value={formValues.body} ></textarea>
                 <input className="create-update" name='image' type='text' placeholder='Image Link' onChange={handleChange} value={formValues.image}></input>
+                <div className="update-close-button">
+                    <button className="submit-button" type='submit'>Submit Changes</button>
+                </div>
             </form> : 
                 <button className='update-button' onClick={() => setUpdate(true)}>Edit Your Project</button>}
         </div>
